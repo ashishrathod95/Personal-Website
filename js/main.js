960e91e6fc1e174b4f24be2f06bf997a1ea3b10e
Ashish Rathod <anusudha.1212@gmail.com>
2021-03-16 19:28:07 +0530

    Skill Section Added in About Section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-const aboutSection = document.querySelector(".about-section"),
-  tabsContainer = document.querySelector(".about-tabs");
+const aboutSection = document.querySelector('.about-section');
+const tabsContainer = document.querySelector('.about-tabs');
 
-tabsContainer.addEventListener("click", (e) => {
-    // console.log(e.target);
-    if(e.target.classList.contains("tab-items") && !e.target.classList.contains("active")) {
-      const target =  e.target.getAttribute("data-target");
-      tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
-      e.target.classList.add("outer-shadow", "active");
-    }
-})
+tabsContainer.addEventListener('click',(event) => {
+  if (event.target.classList.contains('tab-items')) {
+    const target = event.target.getAttribute('data-target');
+    // console.log(target);
+    tabsContainer.querySelector(".active").classList.remove('outer-shadow','active');
+    event.target.classList.add("active","outer-shadow");
+    aboutSection.querySelector(".tab-content.active").classList.remove("active");
+    aboutSection.querySelector(target).classList.add("active");
+  }
+});
